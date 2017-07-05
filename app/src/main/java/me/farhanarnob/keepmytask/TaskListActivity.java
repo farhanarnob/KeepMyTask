fc7d382781856c5f6fd547535e8ce9af71f023ae
Farhan Rahman Arnob <farhanarnob@live.com>
2017-07-05 15:30:08 +0600

    Initial Commit

---
+++ app/src/main/java/me/farhanarnob/keepmytask/TaskListActivity.java
@@ -0,0 +1,13 @@
+package me.farhanarnob.keepmytask;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TaskListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_list);
+    }
+}
---
+++ app/src/main/java/me/farhanarnob/keepmytask/data/TaskContract.java
@@ -0,0 +1,38 @@
+package me.farhanarnob.keepmytask.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Farhan Rahman Arnob on ${06-Oct-16}.
+ * For coding task of INTELLIJ SYSTEM SOLUTION SDN BHD.
+ * Licensed under the Apache License, Version 2.0 ( The "license");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of he License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// API Contract for Keep my task app
+public final class TaskContract {
+
+    // preventing unexpected initializing
+    private TaskContract() {
+    }
+
+    public static final class TaskEntry implements BaseColumns {
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_TASK_NAME = "name";
+        public final static String COLUMN_TASK_DESCRIPTION = "description";
+        public final static String COLUMN_TASK_DATE_CREATED = "dateCreated";
+        public final static String COLUMN_TASK_DATE_UPDATED = "dateUpdated";
+
+    }
+
+
+}
